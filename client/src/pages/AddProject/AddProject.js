dbcdb29ca197b77d444a463480e0d16957609cc7
Jaspreet Kaur <jaspreetr86@gmail.com>
2018-10-11 14:58:18 -0400

    add API route to create project

--- client/src/pages/AddProject/AddProject.js
+++ client/src/pages/AddProject/AddProject.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import { TextArea, Input,FormBtn } from "../../components/Form";
+import { TextArea, Input, FormBtn} from "../../components/Form";
 import Column from "../../components/Column";
 import Container from "../../components/Container";
 import Row from "../../components/Row";
-import 'react-dates/initialize';
-import 'react-dates/lib/css/_datepicker.css';
-import { DateRangePicker } from 'react-dates';
+import DatePicker from "react-datepicker";
+import moment from "moment";
+import 'react-datepicker/dist/react-datepicker.css';
+import API from "../../utils/API";
+import axios from "axios";
 
 class AddProject extends Component {
 
@@ -13,23 +15,125 @@ class AddProject extends Component {
       super(props);
   
       this.state = {
-        // startDate: null,
-        // endDate: null,
         show: false,
-        userInput: {}
+        title: "",
+        description: "",
+        location: "",
+        startDate: null,
+        endDate: null,
+        imageUrl: "",
+        message: "",
+        loading: false
       }
     }
+
+    handleChange = ({startDate, endDate}) => {
+      startDate = startDate || this.state.startDate
+      endDate = endDate || this.state.endDate
+      if (startDate.isAfter(endDate))
+      {
+          endDate = startDate
+      }
+
+      this.setState({ startDate, endDate})
+  }
+
+    handleStart = (startDate) => {
+      this.handleChange({ startDate })
+  }
+
+  handleEnd = (endDate) => {
+      this.handleChange({ endDate })
+  }
   
-    handleShowRadioChange = () => {
-      this.setState({ show: true });
+    dataChange = (e) => {
+      this.setState({
+        [e.target.name]: e.target.value
+      })
     }
-  
-    handleHideRadioChange = () => {
-      this.setState({ show: false });
-    }
-  
-    handleFormValidation = () => {
-        alert("form submitted");
+
+  createProject = (e) => {
+
+      e.preventDefault();
+
+      const title = this.state.title
+      const description = this.state.description
+      const location = this.state.location
+      const startDate = this.state.startDate
+      const endDate = this.state.endDate
+      const imageUrl = this.state.imageUrl
+   
+      const userInput = {
+        title,
+        description,
+        location,
+        startDate,
+        endDate,
+        imageUrl
+      }
+
+      API.createProject({
+        userInput
+      })
+      .then(res => {
+        this.setState({
+          loading:false,
+          message: response.userInput
+        })
+      })
+      .catch(err => {
+        this.setState({
+          loading: false
+        })
+      })
+  }
+
+    // postData = (e) => {
+    //   e.preventDefault();
+
+    //   const title = this.state.title
+    //   const description = this.state.description
+    //   const location = this.state.location
+    //   const startDate = this.state.startDate
+    //   const endDate = this.state.endDate
+    //   const imageUrl = this.state.imageUrl
+
+    //   this.setState({
+    //     loading:true
+    //   })
+
+    //   const userInput = {
+    //     title,
+    //     description,
+    //     location,
+    //     startDate,
+    //     endDate,
+    //     imageUrl
+    //   }
+
+    //   axios.post(" /t/tt0mq-1539193578/post", userInput)
+    //   .then(response => {
+    //     this.setState({
+    //       loading:false,
+    //       message: response.userInput
+    //     })
+    //   })
+    //   .catch(err => {
+    //     this.setState({
+    //       loading: false
+    //     })
+    //   })
+    // }
+
+    loadOrShowMsg = () => {
+      if(this.state.loading)
+      {
+        return <p> Loading... </p>
+      }
+      else
+      {
+        return <p>{this.state.message}</p>
+      }
     }
   
     render() {
@@ -38,46 +142,63 @@ class AddProject extends Component {
           <Container>
             <Row>
               <Column>
-                <form style={{ display: "block" }}>
+                <form onSubmit={this.createProject.bind(this)}>
                   <label>
                     Title:
                     <Input
-                    value={this.state.userInput.title} 
+                    value={this.state.title} 
                     name="title" 
-                    placeholder="Title (Required)" />
+                    placeholder="Title (Required)"
+                    onChange={this.dataChange.bind(this)} />
                   </label>
                   <label>
                     Description:
                     <TextArea 
-                    value={this.state.userInput.description}
+                    value={this.state.description}
+                    onChange={this.dataChange.bind(this)}
                     name="description" placeholder="Description (Required 1000 Characters)" />
                   </label>
                   <label>
                     Location:
                     <Input 
-                    value={this.state.userInput.location}
+                    value={this.state.location}
+                    onChange={this.dataChange.bind(this)}
                     name="location" placeholder="Address or Postal Code (Required)" />
                   </label>
                   <label>
-                    <DateRangePicker
-                      startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-                      value={this.state.userInput.startDate}
-                      startDateId="start_id" // PropTypes.string.isRequired,
-                      endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-                      value={this.state.userInput.endDate}
-                      endDateId="end_id" // PropTypes.string.isRequired,
-                      onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
-                      focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-                      onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
-                    />
+                    Start Date:
+                  <DatePicker
+                            className="rounded p-1" 
+                            selected={this.state.startDate}
+                            selectsStart
+                            startDate={this.state.startDate}
+                            endDate={this.state.endDate}
+                            onChange={this.handleStart}
+                            placeholderText="Start Date"
+                          />
+                  </label>
+                  <label>
+                    End Date:
+                  <DatePicker
+                            className="rounded p-1" 
+                            selected={this.state.endDate}
+                            selectsEnd
+                            startDate={this.state.startDate}
+                            endDate={this.state.endDate}
+                            onChange={this.handleEnd}
+                            placeholderText="End Date"
+                          />
                   </label>
                   <label>
                     Image:
                     <Input 
-                    value={this.state.userInput.imageUrl}
-                    type="text" name="imageUrl" placeholder="Enter Image URL" required />
+                    name="imageUrl"
+                    onChange={this.dataChange.bind(this)}
+                    value={this.state.imageUrl}
+                    type="text" placeholder="Enter Image URL" required />
                   </label>
-                  <label >
+                  
+                  {/* <label >
                     Compensation: {' '}
                     <form style={{ display: "inlineBlock" }}>
                       <label style={{ marginRight: "80px" }}>
@@ -94,13 +215,13 @@ class AddProject extends Component {
                   <label show={this.state.show}>
                     Amount:
                       <Input type="number" name="amount" />
-                  </label>
-                  <FormBtn 
-                  handleFormValidation={this.handleFormValidation}
-                  >
+                  </label> */}
+                  <FormBtn>
                     Post
                   </FormBtn>
                 </form>
+
+                {this.loadOrShowMsg()}
               </Column>
             </Row>
           </Container>

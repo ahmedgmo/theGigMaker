8ce79815bbca6608a94335b587df0512dc74f2b4
Mjay <martin_jarrin@hotmail.com>
2018-10-12 19:00:54 -0400

    updated routes

--- app/routes/project.js
+++ app/routes/project.js
@@ -24,32 +24,33 @@ module.exports = function(router) {
 
 
 
-    // All projects on the database for viewers to see
+    // Gets All projects in the data base
+    // Upon click the id headline belonging to a project is sent
+    // the database sends back specific info for that project
 
-    router.get("/api/get-dbprojects", function (req, res){
-        // var query = {}
-        // if ( req.params.id) {
-        //     query.id = req.params.id;
-        // }
+    router.post("/api/get-dbprojects/:id?", function (req, res){
+        var query = {}
+        if ( req.params.id) {
+            query._id = req.params.id;
+        }
 
-        Project.get( function( data){
-        
-            res.status(200).json(data);
+        Project.get(query, function(err,docs){
+
+            console.log(docs);
+            
+            if(docs){
+
+                res.status(200).json(docs);
+
+            } else {
+                console.log(err);
+                res.redirect("/");
+
+            }
         });
     });
 
 
-
-    // User get access to all his 
-    router.get("/api/savedProjects", function(req, res){
-        // res.render("saved");
-    });
-
-
-    router.get("/api/savedCollaborations", function(req, res){
-        query = req.body;
-        Project.get(query);
-    });
 
 
 
@@ -58,9 +59,14 @@ module.exports = function(router) {
         var query = req.body;
            Project.create(query, function (err, docs, data){
                console.log(docs);
-               if (docs.result.ok == 1) {
-                   console.log(data);
-                    res.status(200).send('Project Created!');
+               if (docs.result.ok) {
+                   console.log("project created");
+                    res.status(200).json(data);
+               } else {
+
+                    console.log(err);
+                    res.redirect('/');
+                    
                }
            })
         });
@@ -77,6 +83,10 @@ module.exports = function(router) {
         Project.delete(query, function (err,data){
             if (data){
                 res.status(200).send('project has been deleted');
+            } else {
+                console.log(err);
+                res.redirect("/");
+
             }
 
         });
@@ -84,10 +94,15 @@ module.exports = function(router) {
 
 
     // update content (creator)
+    // do we need to send back updated info?
     router.put("/api/update-project", function (req, res){
         Project.update(req.body, function (err, data){
             if (data){
                 res.status(200).json("project has been updated");
+            } else {
+                console.log(err);
+                res.redirect("/");
+
             }
 
         });
@@ -96,19 +111,64 @@ module.exports = function(router) {
 
 
 
-    // collaboration and button rendering
-    // object from front end {}
-
-
-
-    router.post("/api/project-collab-pending", function (req, res){
+    // collaboration button
+    // user is linked to the project 
+    // Trello is populated upon acceptance
+    // adding notes
+    router.post("/api/project-collab-pending/:id", function (req, res){
         console.log(req.body)
         var query = req.body;
         
         Project.collab(query, function(err, data){
+        if (data){
             res.json(data);
+        } else {
+            console.log(err);
+            res.redirect("/");
+
+        }
+        });
+    });
+
+    // Creator approval button route
+    router.put("/api/project-collab-pending", function (req, res){
+        console.log(req.body)
+        var query = req.body;
+        
+        Project.approve(query, function(err, data){
+
+        if (data){
+            res.status(200).json(data);
+        } else {
+            console.log(err);
+            res.redirect("/");
+
+        }
         });
     });
     
+    // see all collaborator within a project
+    router.post("/api/get-projectCollaborators/:id?", function (req, res){
+        var query = {}
+        if ( req.params.id) {
+            query._id = req.params.id;
+        }
+
+        Project.getSpecific(query, function(err,docs,data){
+
+            console.log(docs);
+            
+            if(docs){
+
+                res.status(200).json(data);
+
+            } else {
+                console.log(err);
+                res.redirect("/");
+
+            }
+        });
+    });
+
 }
 
